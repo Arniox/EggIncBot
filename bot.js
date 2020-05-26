de2303cd782f4d7ed7fd64706fc0b4f29550d668
Nikkolas Diehl <nikkdiehl@gmail.com>
2020-05-26 15:53:14 +1200

    Added bot.js

---
+++ bot.js
@@ -0,0 +1,70 @@
+//Constst
+const Enmap = require("enmap");
+const Discord = require('discord.js');
+const fs = require('fs');
+const settings = require('./configure.json');
+const dotenv = require('dotenv');
+//Load env variables
+dotenv.config({ path: './.env' });
+
+//Initialise Discord bot
+var bot = new Discord.Client();
+
+//Attach settings to bot
+bot.config = settings;
+
+//Attaches all events files to event
+fs.readdir('./events/', (err, files) => {
+    if (err) return console.error(err); //Throw if file breaks
+    files.forEach(file => {
+        //If file is not js file, ignore
+        if (!file.endsWith('.js')) return;
+        //Load event file
+        const event = require(`./events/${file}`);
+        //Get the event name from file name
+        var eventName = file.split('.')[0];
+        console.log(`Attempting to load event ${eventName}`);
+        //Bind all found events to client
+        bot.on(eventName, event.bind(null, bot));
+        delete require.cache[require.resolve(`./events/${file}`)]; //Remove from memory
+    });
+});
+
+//Set up command sand attach to the client
+bot.commands = new Enmap();
+fs.readdir('./commands/', (err, files) => {
+    if (err) return console.err(err); //Throw if folder breaks
+    files.forEach(file => {
+        //If file is not js file. Ignore
+        if (!file.endsWith('.js')) return;
+        //Load the command file itself
+        var props = require(`./commands/${file}`);
+        //Get just the command name from the file name
+        var commandName = file.split('.')[0];
+        console.log(`Attempting to load command ${commandName}`);
+        //Store the command in the command Enmap.
+        bot.commands.set(commandName, props);
+    });
+});
+//Handle promise rejections
+process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
+//Bot loggin
+bot.login(process.env.BOT_TOKEN);
+
+//---------------PROTOTYPES-----------------------
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); //don't forget the second param
+    var hours = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours < 10)
+        hours = '0' + hours;
+    if (minutes < 10)
+        minutes = '0' + minutes;
+    if (seconds < 10)
+        seconds = '0' + seconds;
+
+    var time = hours + ':' + minutes + ':' + seconds;
+    return time;
+}
