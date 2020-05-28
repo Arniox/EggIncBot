164f0c139ab212dd4084b728d27d738b74b9623f
Nikkolas Diehl <nikkdiehl@gmail.com>
2020-05-28 16:28:11 +1200

    Added guildCreate event file to commit

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -9,9 +9,23 @@ module.exports = (bot, guild) => {
     bot.con.query(sql_cmd, (error, results, fields) => {
         if (error) return console.error(error); //Return error console log and continue
         else if (!results || !results.length) {
-            //Create new
-            const update_cmd = `INSERT INTO configure (Guild_Id, Guild_Name, prefix) VALUES (${guild.id}, "${guild.name}", "e-")`;
-            bot.con.query(update_cmd).catch(error => console.error(error));
+            //Create default mod controller
+            const mod_control_cmd = `
+            INSERT INTO ModControl (ModLevel_Id)
+                VALUES ((
+                    SELECT ModLevel_Id
+                    FROM ModerationLevel
+                    WHERE Name = 'None'
+                    LIMIT 1
+                ))`;
+            bot.con.query(mod_control_cmd, (error, results, fields) => {
+                if (error) return console.error(error); //Throw error and continue
+                //Create new configure
+                const configure_cmd = `
+                INSERT INTO configure (Guild_Id, Guild_Name, Prefix, Mod_Id)
+                    VALUES ("${guild.id}", "${guild.name}", "e-", ${results.insertId})`;
+                bot.con.query(configure_cmd).catch(error => console.error(error));
+            });
         }
     });
 }
